5929ad7acc5a612ab420a28c7154e9d69316489e
RyoSkyKnight <koromoro286@gmail.com>
2024-12-16 11:55:30 +0700

    w

--- src/app/pages/program/page.tsx
+++ src/app/pages/program/page.tsx
@@ -10,35 +10,18 @@ interface FormData {
 export default function ProgramPage() {
 
   const [formData, setFormData] = useState<FormData | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [error] = useState<string | null>(null);
 
   useEffect(() => {
     const savedData = sessionStorage.getItem("formData");
 
     if (savedData) {
       // Ambil dari sessionStorage jika ada
-      setFormData(JSON.parse(savedData));
-    } else {
-      // Jika sessionStorage kosong, ambil data dari API
-      const fetchData = async () => {
-        try {
-          const response = await fetch("/api/program", { method: "GET" });
-          if (!response.ok) throw new Error("Gagal mengambil data dari server");
-
-          const data = await response.json();
-          setFormData(data);
-        } catch (err) {
-          console.error(err);
-          setError("Data tidak tersedia.");
-        }
-      };
-
-      fetchData();
-    }
+      setFormData(JSON.parse(savedData)); }
   }, []);
 
   return (
-    <CustomLayout mainline="Pilih paket program biar makin jago! 🚀." line=" Drives your success from here!✨ #KampungInggrisLC #BestEnvironmentForTheBestResult">
+    <CustomLayout mainline="Pilih paket program yang relevan biar makin jago! 🚀." line=" Drives your success from here!✨ #KampungInggrisLC #BestEnvironmentForTheBestResult">
     
     <div className="p-10">
       <h1 className="text-2xl font-bold mb-4">Data Formulir Anda</h1>
